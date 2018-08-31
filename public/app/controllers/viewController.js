62ae330dc0ef056435efea667f5bbf4d3ec32099
Thanh Ho <hominh@deakin.edu.au>
2018-09-01 02:08:44 +1000

    Add player card and add the click button in the font end

--- public/app/controllers/viewController.js
+++ public/app/controllers/viewController.js
@@ -2,9 +2,28 @@ var app = angular.module("indexApp", ['ngRoute']);
 
 
 //Player controller
-app.controller('playerCtr', function ($scope, $routeParams) {
+app.controller('playerCtr', function ($scope, $routeParams,$http) {
     console.log("i am player "+ $routeParams.playerName);
     $scope.name = $routeParams.playerName;
+    //Search click
+    $scope.AddToMyCollection = function () {
+        console.log("Add to my collection clicked");
+
+        //Request the back-end to store it to the database.
+        $http({
+            method: "post",
+            url: "/AddToMyCollection"
+        }).then(function mySuccess(response) {
+            if (response.data.success) {
+                console.log(response);
+               
+            }
+            else {
+                console.log(response);
+               // $scope.successMsg = "Result could not foud";
+            }
+        });
+    }
 });
 
 
