2406df517780660adcd08a5eb1a4c06662b6cd47
Thanh Ho <hominh@deakin.edu.au>
2018-09-03 12:01:26 +1000

    Add the user in the Navigation Bar that contain My Account and my collection. Add the controller and the pages associate with the link

--- public/app/controllers/viewController.js
+++ public/app/controllers/viewController.js
@@ -1,5 +1,14 @@
 var app = angular.module("indexApp", ['ngRoute']);
 
+//my Account controller
+app.controller('myAccountCtr', function ($scope, $routeParams,$http) {
+    console.log("Account controller");
+});
+
+//my Collection controller
+app.controller('myCollectionCtr', function ($scope, $routeParams,$http) {
+    console.log("Collection controller");
+});
 
 //Player controller
 app.controller('playerCtr', function ($scope, $routeParams,$http) {
@@ -43,6 +52,14 @@ app.config(function ($routeProvider, $locationProvider) {
         .when("/player/:playerName", {
             templateUrl: "app/views/page/player.html",
             controller: 'playerCtr'
+        })
+        .when("/myCollection", {
+            templateUrl: "app/views/page/myCollection.html",
+            controller: 'myCollectionCtr'
+        })
+        .when("/myAccount", {
+            templateUrl: "app/views/page/myAccount.html",
+            controller: 'myAccountCtr'
         })
         .otherwise({ redirectTo: "/" });
     $locationProvider.html5Mode({
