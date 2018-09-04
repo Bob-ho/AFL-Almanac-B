af600c4faa7fa0e13cdef33b71004cb211f2bf61
Thanh Ho <hominh@deakin.edu.au>
2018-09-04 12:58:48 +1000

    add the user interface to User collection. However, the drag and drop function is not working

--- public/app/controllers/viewController.js
+++ public/app/controllers/viewController.js
@@ -8,6 +8,27 @@ app.controller('myAccountCtr', function ($scope, $routeParams,$http) {
 //my Collection controller
 app.controller('myCollectionCtr', function ($scope, $routeParams,$http) {
     console.log("Collection controller");
+
+    $scope.models = {
+        selected: null,
+        lists: {"A": [], "B": []}
+    };
+
+    // Generate initial model
+    for (var i = 1; i <= 10; ++i) {
+        $scope.models.lists.A.push({label: "Item A" + i});
+        $scope.models.lists.B.push({label: "Item B" + i});
+    }
+
+    // Model to JSON for demo purpose
+    $scope.$watch('models', function(model) {
+        $scope.modelAsJson = angular.toJson(model, true);
+    }, true);
+
+
+
+
+
 });
 
 //Player controller
