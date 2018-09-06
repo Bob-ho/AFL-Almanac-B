1c3dab305348c2845f52b4624f6b018105289d42
Thanh Ho <hominh@deakin.edu.au>
2018-09-07 03:26:02 +1000

    Create the pie chart to show how many cart that required user to collect

--- public/app/controllers/viewController.js
+++ public/app/controllers/viewController.js
@@ -39,6 +39,25 @@ app.controller('myAccountCtr', function ($scope, $routeParams, $http) {
 //my Collection controller
 app.controller('myCollectionCtr', function ($scope, $http,$window) {
     $scope.showPlayerDetail = true;
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(drawChart);
+   var a = 1;
+
+    // Draw the chart and set the chart values
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Point', a],
+            ['More Collection', 2]
+        ]);
+
+        // Optional; add a title and set the width and height of the chart
+        var options = { 'title': 'My Average Day'};
+
+        // Display the chart inside the <div> element with id="piechart"
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+        chart.draw(data, options);
+    }
     console.log("Collection controller");
     //Get the job detail which given the _id to retrived data from the database
     $http({
