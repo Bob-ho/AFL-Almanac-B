ba99b5239dcfdc72bacd1b929d26516c31d4fd24
Thanh Ho <hominh@deakin.edu.au>
2018-09-18 11:56:59 +1000

    Add more image in the player page that will allow the user to have more information about the player

--- public/app/controllers/viewController.js
+++ public/app/controllers/viewController.js
@@ -197,38 +197,6 @@ app.controller('registerController', function ($scope, $http, $location) {
 //Player controller
 app.controller('playerCtr', function ($scope, $routeParams, $http, $window, $location) {
 
-    var slideIndex = 1;
-    showDivs(slideIndex);
-
-    $scope.plusDivs = function (n) {
-        console.log(n);
-        showDivs(slideIndex += n);
-    }
-
-    $scope.currentDiv = function (n) {
-        showDivs(slideIndex = n);
-    }
-
-    function showDivs(n) {
-        var i;
-        var x = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("demo");
-        if (n > x.length) { slideIndex = 1 }
-        if (n < 1) { slideIndex = x.length }
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" w3-red", "");
-        }
-        x[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " w3-red";
-    }
-
-
-
-
-
     console.log("i am player " + $routeParams.playerID);
     // $scope.name = $routeParams.playerID;
     //get the url including the data parsing
